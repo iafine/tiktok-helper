cbfa41f0bceaba6d81e948edf88319938e7315d0
iafine <iafineden@gmail.com>
2022-07-10 19:59:49 +0800

    feat: 添加tiktok视频解析

--- options/components/Form.tsx
+++ options/components/Form.tsx
@@ -1,72 +1,97 @@
-import { Button, Col, Form, Row, Upload } from "antd"
+import { Button, Col, Form, Input, Row, Typography } from "antd"
 import { useState } from "react"
-import { AiOutlineInbox } from "react-icons/ai"
 
-import { userIsLogin } from "../../utils"
+import { parseTikTok } from "../../utils"
 import { RoundLayout } from "./RoundLayout"
 
 const { useForm } = Form
 
 export const BusinessForm = () => {
   const [submitLoading, setSubmitLoading] = useState(false)
-  const [fileList, setFileList] = useState<any[]>([])
+  const [videoParams, setVideoParams] = useState({}) as any
+
   const [form] = useForm()
-  const formInitialValues = { fileList: [] }
 
-  const onUploadViedeos = async () => {
+  const onFormFinish = async (data) => {
+    setVideoParams({})
     setSubmitLoading(true)
-    console.log(fileList)
-    console.log(chrome)
-
-    await userIsLogin()
-
+    const { link = "" } = data
+    try {
+      const params = await parseTikTok(link)
+      console.log(params)
+      setVideoParams(params)
+    } catch (error) {
+      console.error(error)
+    }
     setSubmitLoading(false)
   }
 
-  const uploadProps: any = {
-    onRemove: (file) => {
-      const index = fileList.indexOf(file)
-      const newFileList = fileList.slice()
-      newFileList.splice(index, 1)
-      setFileList(newFileList)
-    },
-    beforeUpload: (file) => {
-      setFileList([...fileList, file])
-
-      return false
-    },
-    fileList,
-    directory: true
+  const downloadResource = (url: string) => {
+    const eleLink = document.createElement("a")
+    eleLink.style.display = "none"
+    eleLink.href = url
+    document.body.appendChild(eleLink)
+    eleLink.click()
+    document.body.removeChild(eleLink)
   }
 
   return (
-    <RoundLayout title="发布视频">
-      <div className="flex flex-col">
-        <span>批量视频文件夹</span>
-        <Upload {...uploadProps}>
-          <div className="flex justify-center items-center w-full">
-            <div className="flex flex-col justify-center items-center w-full h-64 bg-gray-50 rounded-lg border-2 border-gray-300 border-dashed cursor-pointer dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
-              <div className="flex flex-col justify-center items-center pt-5 pb-6">
-                <AiOutlineInbox className="mb-3 w-10 h-10 text-gray-400" />
-                <p className="mb-2 text-gray-500 text-base">
-                  <span className="font-semibold">点击这里上传视频集合</span>{" "}
-                  或者将文件夹拖入这里
-                </p>
-                <p className=" text-gray-500 text-xl">
-                  注意：请提前确认好文件夹内的东西内容，否则会导致批量上传失败
-                </p>
-              </div>
+    <RoundLayout title="TikTok视频解析">
+      <Form form={form} onFinish={onFormFinish} layout="vertical" size="middle">
+        <Row>
+          <Col span={12}>
+            <Form.Item
+              name="link"
+              label="链接"
+              rules={[{ required: true, message: "请输入链接" }]}>
+              <Input placeholder="请输入链接" />
+            </Form.Item>
+          </Col>
+        </Row>
+        <Form.Item>
+          <Button type="primary" htmlType="submit" loading={submitLoading}>
+            解析视频
+          </Button>
+        </Form.Item>
+      </Form>
+      <Row>
+        {videoParams.nwmVideoUrl && (
+          <Col span={12}>
+            <Typography.Title level={5}>
+              {videoParams.videoTitle}
+            </Typography.Title>
+            <div>
+              <Typography.Text type="secondary">
+                作者：
+                <Typography.Text>{videoParams.authorNickname}</Typography.Text>
+              </Typography.Text>
             </div>
-          </div>
-        </Upload>
-      </div>
-      <Button
-        type="primary"
-        htmlType="submit"
-        loading={submitLoading}
-        onClick={onUploadViedeos}>
-        发布
-      </Button>
+            <div>
+              <Typography.Text type="secondary">
+                创建日期：
+                <Typography.Text>{videoParams.createTime}</Typography.Text>
+              </Typography.Text>
+            </div>
+            <div className="my-3">
+              <video className="w-full max-h-80" controls width="250">
+                <source src={videoParams.nwmVideoUrl} type="video/mp4" />
+                Sorry, your browser doesn't support embedded videos.
+              </video>
+            </div>
+            <Button
+              type="primary"
+              onClick={() => downloadResource(videoParams.nwmVideoUrl)}>
+              下载视频
+            </Button>
+            <Button
+              className="ml-3"
+              type="primary"
+              onClick={() => downloadResource(videoParams.musicUrl)}>
+              下载音频
+            </Button>
+          </Col>
+        )}
+      </Row>
     </RoundLayout>
   )
 }
--- utils/index.ts
+++ utils/index.ts
@@ -1,10 +1,121 @@
 import axios from "axios"
+import { get } from "lodash"
 
-// 判断用户是否登录
-export const userIsLogin = async () => {
-  const { data } = await axios.post(
-    "https://channels.weixin.qq.com/cgi-bin/mmfinderassistant-bin/auth/auth_data",
-    { headers: { "wtf-Origin": "https://channels.weixin.qq.com" } }
+const TIKTOK_URL = "https://api.tiktokv.com/aweme/v1/multi/aweme/detail/"
+
+// 解析TikTok视频
+export const parseTikTok = async (url: string) => {
+  if (!url.includes("https://www.tiktok.com")) {
+    console.error("错误的TikTok链接")
+    return
+  }
+  const matchList = url.match(/\d+/g)
+  if (!matchList[0]) {
+    console.error("未匹配到视频ID")
+    return
+  }
+
+  // https://api.tiktokv.com/aweme/v1/multi/aweme/detail/?aweme_ids=%5B7104679681894386950%5D
+
+  const res = await axios.get(
+    `${TIKTOK_URL}?aweme_ids=${encodeURI(`[${matchList[0]}]`)}`,
+    {
+      // headers: {
+      //   "user-agent":
+      //     "Mozilla/5.0 (Linux; Android 8.0; Pixel 2 Build/OPD3.170816.012) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Mobile Safari/537.36 Edg/87.0.664.66"
+      // }
+    }
   )
-  console.log(data)
+  const { data = {} } = res || {}
+  // 无水印视频链接
+  const nwmVideoUrl = get(
+    data,
+    "aweme_details[0].video.play_addr.url_list[0]",
+    ""
+  )
+  // 有水印链接
+  const wmVideoUrl = get(
+    data,
+    "aweme_details[0].video.download_addr.url_list[0]",
+    ""
+  )
+  // 视频标题
+  const videoTitle = get(data, "aweme_details[0].desc", "")
+  //作者昵称
+  const authorNickname = get(data, "aweme_details[0].author.nickname", "")
+  // 作者ID
+  const authorId = get(data, "aweme_details[0].author.uid", "")
+  // 上传时间
+  const createTime = get(data, "aweme_details[0].create_time", "")
+  // 视频ID
+  const videoId = get(data, "aweme_details[0].statistics.aweme_id", "")
+  // 视频音乐标题
+  const musicTitle = get(data, "aweme_details[0].music.title", "")
+  // 视频音乐ID
+  const musicId = get(data, "aweme_details[0].music.mid", "")
+  // 视频音乐作者
+  const musicAuthor = get(data, "aweme_details[0].music.author", "")
+  // 视频音乐链接
+  const musicUrl = get(data, "aweme_details[0].music.play_url.url_list[0]", "")
+  // 评论数
+  const commentCount = get(
+    data,
+    "aweme_details[0].statistics.comment_count",
+    ""
+  )
+  // 点赞数
+  const diggCount = get(data, "aweme_details[0].statistics.digg_count", "")
+  // 播放次数
+  const playCount = get(data, "aweme_details[0].statistics.play_count", "")
+  // 下载次数
+  const downloadCount = get(
+    data,
+    "aweme_details[0].statistics.download_count",
+    ""
+  )
+  // 分享次数
+  const shareCount = get(data, "aweme_details[0].statistics.share_count", "")
+  // 视频封面
+  const coverUrl = get(data, "aweme_details[0].cover.url_list[0]", "")
+  // 视频动态封面
+  const dynamicCoverUrl = get(
+    data,
+    "aweme_details[0].dynamic_cover.url_list[0]",
+    ""
+  )
+  // 视频原始封面
+  const originCoverUrl = get(
+    data,
+    "aweme_details[0].origin_cover.url_list[0]",
+    ""
+  )
+  //  视频话题
+  const topics = get(data, "aweme_details[0].text_extra", [])
+    .map(({ hashtag_name }) => {
+      return hashtag_name
+    })
+    .filter((tag) => tag)
+
+  return {
+    nwmVideoUrl,
+    wmVideoUrl,
+    videoTitle,
+    authorNickname,
+    authorId,
+    createTime,
+    videoId,
+    musicTitle,
+    musicId,
+    musicAuthor,
+    musicUrl,
+    commentCount,
+    diggCount,
+    playCount,
+    downloadCount,
+    shareCount,
+    coverUrl,
+    dynamicCoverUrl,
+    originCoverUrl,
+    topics
+  }
 }
