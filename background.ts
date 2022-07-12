5be5e0607b360728f060cee3849e23e294ee0268
iafine <iafineden@gmail.com>
2022-07-12 18:15:57 +0800

    feat: 添加快捷键支持

--- background.ts
+++ background.ts
@@ -6,30 +6,45 @@ console.log("=============background start=============")
 
 const storage = new Storage()
 
-// 点击action logo
-chrome.action.onClicked.addListener(async (tab) => {
-  const { url = "" } = tab
+// 打开解析视频页面
+export const openParseVideoPage = async (url: string) => {
   if (url.includes("https://www.tiktok.com/@")) {
     await storage.set("__TIKTOK_URL__", url)
   } else {
     await storage.set("__TIKTOK_URL__", "")
   }
   chrome.runtime.openOptionsPage()
+}
+
+// 获取当前tab
+export const getCurrentTab = async () => {
+  let queryOptions = { active: true, lastFocusedWindow: true }
+  let [tab] = await chrome.tabs.query(queryOptions)
+  return tab
+}
+
+// 点击action logo
+chrome.action.onClicked.addListener(async (tab) => {
+  const { url = "" } = tab
+  await openParseVideoPage(url)
 })
 
 // 右键菜单
-chrome.contextMenus.create({
-  id: "tiktok_parse_page",
-  title: chrome.i18n.getMessage("contextMenusPage"),
-  contexts: ["page"],
-  documentUrlPatterns: ["https://www.tiktok.com/@*"]
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.contextMenus.create({
+    id: "tiktok_parse_page",
+    title: chrome.i18n.getMessage("contextMenusPage"),
+    contexts: ["page"],
+    documentUrlPatterns: ["https://www.tiktok.com/@*"]
+  })
+  chrome.contextMenus.create({
+    id: "tiktok_parse_video",
+    title: chrome.i18n.getMessage("contextMenusVideo"),
+    contexts: ["video"],
+    documentUrlPatterns: ["https://www.tiktok.com/@*"]
+  })
 })
-chrome.contextMenus.create({
-  id: "tiktok_parse_video",
-  title: chrome.i18n.getMessage("contextMenusVideo"),
-  contexts: ["video"],
-  documentUrlPatterns: ["https://www.tiktok.com/@*"]
-})
+
 chrome.contextMenus.onClicked.addListener(async (info, tab) => {
   console.log(info, tab)
   const { menuItemId } = info
@@ -38,9 +53,16 @@ chrome.contextMenus.onClicked.addListener(async (info, tab) => {
     menuItemId === "tiktok_parse_video"
   ) {
     const { pageUrl = "" } = info
-    if (pageUrl.includes("https://www.tiktok.com/@")) {
-      await storage.set("__TIKTOK_URL__", pageUrl)
-      chrome.runtime.openOptionsPage()
-    }
+    await openParseVideoPage(pageUrl)
+  }
+})
+
+// 快捷键
+chrome.commands.onCommand.addListener(async (command) => {
+  console.log(`Command: ${command}`)
+  if (command === "open_parse_video") {
+    const tab = await getCurrentTab()
+    const { url = "" } = tab
+    await openParseVideoPage(url)
   }
 })
